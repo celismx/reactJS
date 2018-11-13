0e8e45761765ff11fbc9d4fca0787d54517f5f5b
celismx <celismx@gmail.com>
2018-11-13 13:23:21 +0100

    reactJS-01

---
+++ Modulo1/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const app = React.createElement('div', null, 'Olá');
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(app, rootElement);
